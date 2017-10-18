f93b54d95c1a6cffc6ed4c7269fb2911cd6f7af0
CruyeEblon <cruyeeblon@gmail.com>
2017-10-18 20:03:42 +0000

    Added nod and camera threads

--- beo_threads.py
+++ beo_threads.py
@@ -1,6 +1,7 @@
 """Activates arm mirror, changes eyes and says a short sentence every few seconds"""
 from time import sleep
 from random import randint
+import subprocess
 import threading
 from Movements import Movements
 from Audio import Audio
@@ -36,19 +37,39 @@ def speak():
         AUDIO.speak(sentences[randint(0, 2)])
         sleep(15)
 
+def nod():
+    """Moves beo's had back and forth every few seconds"""
+    while True:
+        MOVEMENTS.set_raw_angle(7, 52)
+        sleep(2)
+        MOVEMENTS.set_raw_angle(7, 0)
+        sleep(2)
+
+def camera():
+    """Takes a picture every minute"""
+    while True:
+        subprocess.check_output(['fswebcam', 'image.jpg'])
+        sleep(60)
+
 def main():
     """Main function, creates the threads"""
     thread1 = threading.Thread(target=arm_mirror)
     thread2 = threading.Thread(target=eye_change)
     thread3 = threading.Thread(target=speak)
+    thread4 = threading.Thread(target=nod)
+    thread5 = threading.Thread(target=camera)
     #Starts the threads.
     thread1.start()
     thread2.start()
     thread3.start()
+    thread4.start()
+    thread5.start()
     #Joins the threads
     thread1.join()
     thread2.join()
     thread3.join()
+    thread4.join()
+    thread5.join()
 
 if __name__ == "__main__":
     main()
